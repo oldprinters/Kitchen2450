588955e6eb11fd78fdd864066b26105e07118ca8
oldprinters <zakaz@oldprinters.ru>
2024-06-20 20:08:31 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include "Timer.h"
+
+Timer tRadar(1000);
+
+//------------------------------------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(115200); //Feedback over Serial Monitor
+  Serial.println(F("=========================== ptr =="));
+  delay(500);
+  Serial2.begin (256000, SERIAL_8N1, 16, 17); //UART for monitoring the radar
+  delay(500);
+}
+//------------------------------------------------------------------------------------------------------------
+int getRadar(){
+  int i = 0;
+  while(Serial2.available()){
+    if (Serial2.read()==0xAA && Serial2.read()==0xFF) {
+      Serial.println("Radar: ");
+      for (i = 0; i < 28; i++) {
+        Serial.print(Serial2.read(), HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+      }
+  }
+  return i;
+}
+//------------------------------------------------------------------------------------------------------------
+void loop() {
+  if(tRadar.getTimer()){
+    tRadar.setTimer();
+    getRadar();
+  }
+  // put your main code here, to run repeatedly:
+}
